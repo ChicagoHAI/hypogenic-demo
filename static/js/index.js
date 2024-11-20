70cc7482db33f85f6efab13f1a32f0d22783e14f
laoliu5280 <haokunliu@uchicago.edu>
2024-11-20 14:30:34 -0600

    fix maybe?

--- static/js/index.js
+++ static/js/index.js
@@ -4,25 +4,18 @@ window.HELP_IMPROVE_VIDEOJS = false;
 $(document).ready(function() {
     // Check for click events on the navbar burger icon
 
-    // var options = {
-	// 		slidesToScroll: 1,
-	// 		slidesToShow: 1,
-	// 		loop: true,
-	// 		infinite: true,
-	// 		autoplay: false,
-	// 		autoplaySpeed: 20000,
-    // }
+    var options = {
+			slidesToScroll: 1,
+			slidesToShow: 1,
+			loop: true,
+			infinite: true,
+			autoplay: false,
+			autoplaySpeed: 30000,
+    }
 
-	// 	// Initialize all div with carousel class
-    // var carousels = bulmaCarousel.attach('.carousel', options);
+		// Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.carousel', options);
 	
-    // bulmaSlider.attach();
-	bulmaCarousel.attach('#text-carousel', {
-		slidesToScroll: 1,
-		slidesToShow: 1,
-		autoplay: false,
-		autoplaySpeed: 20000,
-		infinite: true,
-	  });
+    bulmaSlider.attach();
 
 })
